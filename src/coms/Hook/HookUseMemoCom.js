1479d2e37167c07574b673f2af7baf9f235c928e
seokjoon <seokjoon@gmail.com>
2021-05-15 19:17:47 +0900

    hook useMemo

---
+++ src/coms/Hook/HookUseMemoCom.js
@@ -0,0 +1,38 @@
+import React, { useMemo, useState } from 'react'
+
+const HookUseMemoCom = () => {
+
+  const [list, setList] = useState([])
+  const [num, setNum] = useState('')
+
+  const _getAvg = nums => {
+    console.log('getAvg')
+    if(nums.length === 0) return 0
+    const sum = nums.reduce((a, b) => a + b)
+    return sum / nums.length
+  }
+
+  const avg = useMemo(() => _getAvg(list), [list])
+
+  const onChange = e => {
+    setNum(e.target.value)
+  }
+
+  const onInsert = () => {
+    const nextList = list.concat(parseInt(num))
+    setList(nextList)
+    setNum('')
+  }
+
+  return (
+    <div>
+      <input value={num} onChange={onChange} />
+      <button onClick={onInsert}>insert</button>
+
+      {/*<div>{_getAvg(list)}</div>*/}
+      <div>{avg}</div>
+    </div>
+  )
+}
+
+export default HookUseMemoCom
--- src/views/Hook/HookView.js
+++ src/views/Hook/HookView.js
@@ -3,11 +3,13 @@ import HookUseStateCom from '../../coms/Hook/HookUseStateCom'
 import HookUseEffectCom from '../../coms/Hook/HookUseEffectCom'
 import HookUseReducerCom from '../../coms/Hook/HookUseReducerCom'
 import HookUseReducerInputCom from '../../coms/Hook/HookUseReducerInputCom'
+import HookUseMemoCom from '../../coms/Hook/HookUseMemoCom'
 
 const HookView = () => {
 
   return (
     <div>
+      <HookUseMemoCom />
       <HookUseReducerInputCom />
       <HookUseReducerCom />
       <HookUseEffectCom />
