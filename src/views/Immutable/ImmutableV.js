ba074d1b68f9db06df4509f1009781a6e2014e56
seokjoon <seokjoon@gmail.com>
2021-06-12 21:53:51 +0900

    imm init

---
+++ src/views/Immutable/ImmutableV.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const ImmutableV = () => {
+  return (
+    <div>
+      <h3>ImmutableV</h3>
+      <ul>
+
+      </ul>
+      <div>
+
+      </div>
+    </div>
+  )
+}
+
+export default ImmutableV
--- src/views/Route/RouteV.js
+++ src/views/Route/RouteV.js
@@ -6,6 +6,7 @@ import HookV from '../Hook/HookV'
 import RouteIndexV from './RouteIndexV'
 import RouteRouteV from './RouteRouteV'
 import StyleV from '../Style/StyleV'
+import ImmutableV from '../Immutable/ImmutableV'
 
 const RouteV = () => {
 
@@ -17,6 +18,7 @@ const RouteV = () => {
         <ul>
           <li> <Link to={path}>Index</Link> </li>
           <li> <Link to={path + '/hook'}>Hook</Link> </li>
+          <li> <Link to={path + '/immutable'}>Immutable</Link> </li>
           <li> <Link to={path + '/optima'}>Optima</Link> </li>
           <li> <Link to={path + '/route'}>Route</Link> </li>
           <li> <Link to={path + '/style'}>Style</Link> </li>
@@ -25,6 +27,7 @@ const RouteV = () => {
       <div style={{float:'left', width: '70%',}}>
         <Route path={path + '/'} component={RouteIndexV} exact />
         <Route path={path + '/hook'} component={HookV} />
+        <Route path={path + '/immutable'} component={ImmutableV} />
         <Route path={path + '/optima'} component={OptimaV} />
         <Route path={path + '/route'} component={RouteRouteV} />
         <Route path={path + '/style'} component={StyleV} />
