a120fc3b2b19d71bbc6fa5ed6ce33eea2a338ea6
seokjoon <seokjoon@gmail.com>
2021-06-12 20:41:08 +0900

    styleCssMod

--- src/views/Style/StyleCssModV.js
+++ src/views/Style/StyleCssModV.js
@@ -1,13 +1,22 @@
 import React from 'react'
+import styles from './styleCssModFoo.module.css'
 
 const StyleCssModV = () => {
-
-
-
   return (
     <div>
       <h3>StyleCssModV</h3>
-      
+      <div className={styles.foo}>
+        foo
+      </div>
+      <div className={styles.foo + ' ' + styles.bar}>
+        bar
+      </div>
+      <div className={`${styles.foo} ${styles.bar}`}>
+        bar
+      </div>
+      <div className={[styles.foo, styles.bar].join(' ')}>
+        bar
+      </div>
     </div>
   )
 }
