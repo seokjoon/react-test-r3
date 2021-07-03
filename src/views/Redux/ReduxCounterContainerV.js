4d2da868c0b98802d2427b308931b6460c9517c0
seokjoon <seokjoon@gmail.com>
2021-07-03 09:21:33 +0900

    redux

--- src/views/Redux/ReduxCounterContainerV.js
+++ src/views/Redux/ReduxCounterContainerV.js
@@ -1,5 +1,25 @@
 import React from 'react'
 import ReduxCounterComV from './ReduxCounterComV'
+import { connect } from 'react-redux'
+import { decrease, increase } from '../../redux/ducks/reduxCounterRedux'
+// import { bindActionCreators } from 'redux'
+
+// const mapDispatchToProps = dispatch => ({
+//   decrease: () => dispatch(decrease()),
+//   increase: () => dispatch(increase()),
+// })
+// const mapDispatchToProps = dispatch => bindActionCreators({
+//   decrease,
+//   increase,
+// }, dispatch)
+const mapDispatchToProps = {
+  decrease,
+  increase,
+}
+
+const mapStateToProps = state => ({
+  num: state.reduxCounterRedux.num,
+})
 
 const ReduxCounterContainerV = ({ num, decrease, increase }) => {
   return (
@@ -7,6 +27,4 @@ const ReduxCounterContainerV = ({ num, decrease, increase }) => {
   )
 }
 
-
-
-export default ReduxCounterContainerV
+export default connect(mapStateToProps, mapDispatchToProps)(ReduxCounterContainerV)
--- src/views/Redux/ReduxCounterV.js
+++ src/views/Redux/ReduxCounterV.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import ReduxCounterComV from './ReduxCounterComV'
 import ReduxCounterContainerV from './ReduxCounterContainerV'
 
 const ReduxCounterV = () => {
@@ -7,7 +6,6 @@ const ReduxCounterV = () => {
     <div>
       <h3>ReduxCounterV</h3>
       <div>
-        <ReduxCounterComV num={0} />
         <ReduxCounterContainerV />
       </div>
     </div>
