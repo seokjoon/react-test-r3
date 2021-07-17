9120525ef96d3bfd62732f8e49ec9fd55a5288e5
seokjoon <seokjoon@gmail.com>
2021-07-17 11:40:32 +0900

    route

--- src/views/Hook/HookV.js
+++ src/views/Hook/HookV.js
@@ -6,18 +6,34 @@ import HookUseReducerInputV from './HookUseReducerInputV'
 import HookUseMemoV from './HookUseMemoV'
 import HookUseCallbackV from './HookUseCallbackV'
 import HookUseCustomV from './HookUseCustomV'
+import r3Const from '../../helpers/r3Const'
+import { Link, Route } from 'react-router-dom'
 
 const HookV = () => {
 
+  const path = r3Const.pathPublic + '/hook'
+
   return (
     <div>
-      <HookUseCustomV />
-      <HookUseCallbackV />
-      <HookUseMemoV />
-      <HookUseReducerInputV />
-      <HookUseReducerV />
-      <HookUseEffectV />
-      <HookUseStateV />
+      <h3>HookV</h3>
+      <ul>
+        <li><Link to={path + '/useCustom'}>HookUseCustom</Link></li>
+        <li><Link to={path + '/useCallback'}>HookUseCallback</Link></li>
+        <li><Link to={path + '/useMemo'}>HookUseMemo</Link></li>
+        <li><Link to={path + '/useReducerInput'}>HookUseReducerInput</Link></li>
+        <li><Link to={path + '/useReducer'}>HookUseReducer</Link></li>
+        <li><Link to={path + '/useEffect'}>HookUseEffect</Link></li>
+        <li><Link to={path + '/useState'}>HookUseState</Link></li>
+      </ul>
+      <div>
+        <Route path={path + '/useCustom'} component={HookUseCustomV} />
+        <Route path={path + '/useCallback'} component={HookUseCallbackV} />
+        <Route path={path + '/useMemo'} component={HookUseMemoV} />
+        <Route path={path + '/useReducerInput'} component={HookUseReducerInputV} />
+        <Route path={path + '/useReducer'} component={HookUseReducerV} />
+        <Route path={path + '/useEffect'} component={HookUseEffectV} />
+        <Route path={path + '/useState'} component={HookUseStateV} />
+      </div>
     </div>
   )
 }
--- src/views/Optima/OptimaV.js
+++ src/views/Optima/OptimaV.js
@@ -1,12 +1,24 @@
 import React from 'react'
 import OptimaListV from './OptimaListV'
 import OptimaScrollV from './OptimaScrollV'
+import r3Const from '../../helpers/r3Const'
+import { Link, Route } from 'react-router-dom'
 
 const OptimaV = () => {
+
+  const path = r3Const.pathPublic + '/optima'
+
   return (
     <div>
-      <OptimaScrollV />
-      <OptimaListV />
+      <h3>OptimaV</h3>
+      <ul>
+        <li> <Link to={path + '/scroll'}>OptimaScroll</Link></li>
+        <li> <Link to={path + '/list'}>OptimaList</Link></li>
+      </ul>
+      <div>
+        <Route path={path + '/scroll'} component={OptimaScrollV} />
+        <Route path={path + '/list'} component={OptimaListV} />
+      </div>
     </div>
   )
 }
