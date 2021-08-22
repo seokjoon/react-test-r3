27ead8a43c2f96190c6de25fed8a2a7a3fe3c132
seokjoon <seokjoon@gmail.com>
2021-08-22 12:43:57 +0900

    env

--- src/helpers/r3Client.js
+++ src/helpers/r3Client.js
@@ -5,7 +5,7 @@ const { REACT_APP_HOST_API_KOA, REACT_APP_PATH } = process.env
 
 
 const r3Client = axios.create()
-r3Client.defaults.baseURL = REACT_APP_HOST_API_KOA + REACT_APP_PATH
+r3Client.defaults.baseURL = REACT_APP_HOST_API_KOA //+ REACT_APP_PATH
 r3Client.defaults.headers.common['Authorization'] = 'Bearer foo'
 r3Client.defaults.withCredentials = true
 
