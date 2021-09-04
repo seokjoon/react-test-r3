707609fd9491677170bc4b590e6b178f3e8d446e
seokjoon <seokjoon@gmail.com>
2021-09-04 13:18:35 +0900

    article

--- src/views/Article/ArticleTagV.js
+++ src/views/Article/ArticleTagV.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useCallback, useState } from 'react'
 import styled from 'styled-components'
 
 
@@ -10,26 +10,59 @@ const TagListBlock = styled.div``
 
 const TagListItemBlock = styled.div``
 
+
+//tag 값이 바뀔때만 리렌더링
+const TagListItem = React.memo(({ onDelete, tag }) => (
+  <TagListItemBlock onClick={() => onDelete(tag)}>
+    #{ tag }
+  </TagListItemBlock>
+))
+
+//tags 값이 바뀔 때만 리렌더링
+const TagList = React.memo(({ onDelete, tags }) => (
+  <TagListBlock>
+    { tags.map(tag => <TagListItem key={tag} onDelete={onDelete} tag={tag} />) }
+  </TagListBlock>
+))
+
+
 const ArticleTagV = () => {
 
-  //tag 값이 바뀔때만 리렌더링
-  const TagListItem = React.memo(({ tag }) => <TagListItemBlock>#{ tag }</TagListItemBlock>)
+  const [input, setInput] = useState('')
+  const [tagsLocal, setTagsLocal] = useState([])
 
-  //tags 값이 바뀔 때만 리렌더링
-  const TagList = React.memo(({ tags }) => (
-    <TagListBlock>
-      { tags.map(tag => <TagListItem key={tag} tag={tag} />) }
-    </TagListBlock>
-  ))
+  const addTag = useCallback(tag => {
+    if(!(tag)) return 0
+    if(tagsLocal.includes(tag)) return 0
+    setTagsLocal([...tagsLocal, tag])
+  }, [tagsLocal])
+
+  const onChange = useCallback(e => {
+    setInput(e.target.value)
+  }, [])
+
+  const onDelete = useCallback(tag => {
+    setTagsLocal(tagsLocal.filter(t => t !== tag))
+  }, [tagsLocal])
+
+  const onSubmit = useCallback(e => {
+    e.preventDefault()
+    addTag(input.trim())
+    setInput('')
+  }, [input, addTag])
 
   return (
     <TagBlock>
       <h3>태그</h3>
-      <TagFormBlock>
-        <input placeholder="태그를 입력하세요." />
+      <TagFormBlock onSubmit={onSubmit}>
+        <input
+          onChange={onChange}
+          placeholder="태그를 입력하세요."
+          value={input}
+        />
         <button type="submit">add</button>
       </TagFormBlock>
-      <TagList tags={['foo', 'bar',]} />
+      <TagList onDelete={onDelete} tags={[tagsLocal]} />
     </TagBlock>
   )
 }
