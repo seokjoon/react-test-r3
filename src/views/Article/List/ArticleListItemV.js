21ac220e4085fd83054fd246de28bc597209db58
seokjoon <seokjoon@gmail.com>
2021-10-09 19:41:18 +0900

    article

--- src/views/Article/List/ArticleListItemV.js
+++ src/views/Article/List/ArticleListItemV.js
@@ -2,20 +2,21 @@ import React from 'react'
 import styled from 'styled-components'
 import ArticleDescV from '../ArticleDescV'
 import ArticleTagsV from '../ArticleTagsV'
+import { Link } from 'react-router-dom'
 
 
 const ItemBlock = styled.div``
 
 const ArticleListItemV = ({ article }) => {
 
-  // const { content, dateCreate, id, tags, title, user, } = article
+  const { content, dateCreate, id, tags, title, user, } = article
 
   return (
     <ItemBlock>
-      <h2>제목</h2>
-      <ArticleDescV dateCreate={new Date()} username="username" />
-      <ArticleTagsV tags={['foo', 'bar',]} />
-      <p>foo foo</p>
+      <h2> <Link to="">{ title }</Link> </h2>
+      <ArticleDescV dateCreate={ dateCreate } username={ user.username } />
+      <ArticleTagsV tags={ tags } />
+      <p>{ content }</p>
     </ItemBlock>
   )
 }
